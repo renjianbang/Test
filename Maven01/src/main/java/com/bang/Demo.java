9e3cfd0b928dcbdc24374c3e4ff37ae7bdbbd487
renhaoshuai <1115189918@qq.com>
2021-11-21 18:23:54 +0800

    master commit1

--- Maven01/src/main/java/com/bang/Demo.java
+++ Maven01/src/main/java/com/bang/Demo.java
@@ -4,6 +4,7 @@ public class Demo {
     public String say(String name) {
         System.out.println("hello " + name);
         System.out.println("change");
+        System.out.println("master change");
         return "hello " + name;
 
     }
